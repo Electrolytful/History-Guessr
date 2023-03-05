d9134235dce9943f8216678e1cb3a066b7fa8662
Electrolytful <erhanulusal@hotmail.com>
2023-03-05 18:31:56 +0000

    added the questions and questions/random route on the server

--- server/server.js
+++ server/server.js
@@ -1,9 +1,12 @@
+// import cors and express server as well as the questions.json file holding all the questions 
 const cors = require('cors');
 const express = require('express');
 const questions = require('./questions.json');
 
+// initialise app as the express server
 const app = express();
 
+// make the server use cors and express' JSON compatibility
 app.use(cors());
 app.use(express.json());
 
@@ -13,16 +16,30 @@ app.get('/', async (req, res) => {
     res.status(200).send('Hello, History Guessr!');
 })
 
+// questions route which returns the array holding all the questions from the questions.json file
 app.get('/questions', async (req, res, next) => {
-    if(!questions.length) {
-        res.status().send();
+    // check to see if the questions file or array exists, if it does then send the whole array
+    if(!questions || !questions.length) {
+        res.status(404).send();
     } else {
-        res.status().send(questions);
+        res.status(200).send(questions);
     }
+
+    next();
 })
 
+// route to get a random question from the array of questions
 app.get('/questions/random', async (req, res, next) => {
-    
+    // assigning a random number in the range of the number of elements in the questions array
+    let number = Math.floor(Math.random() * questions.length);
+
+    if(!questions || !questions.length) {
+        res.status(404).send();
+    } else {
+        res.status(200).send(questions[number]);
+    }
+
+    next();
 })
 
 module.exports = app;
