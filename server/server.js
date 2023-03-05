761dc7b130621ac7d34f67c929560fcd95e11f55
Electrolytful <erhanulusal@hotmail.com>
2023-03-05 18:07:13 +0000

    created questions.json file with questions to be used

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const cors = require('cors');
 const express = require('express');
+const questions = require('./questions.json');
 
 const app = express();
 
@@ -8,8 +9,20 @@ app.use(express.json());
 
 
 // Home route
-app.get('/', async (req, res, next) => {
+app.get('/', async (req, res) => {
     res.status(200).send('Hello, History Guessr!');
+})
+
+app.get('/questions', async (req, res, next) => {
+    if(!questions.length) {
+        res.status().send();
+    } else {
+        res.status().send(questions);
+    }
+})
+
+app.get('/questions/random', async (req, res, next) => {
+    
 })
 
 module.exports = app;
