c25fc7e386af3670d7e0a079696cb4ab10b90e99
Electrolytful <erhanulusal@hotmail.com>
2023-03-06 12:06:44 +0000

    GET request for random question now fixed to not repeat recently sent questions

--- server/server.js
+++ server/server.js
@@ -28,6 +28,7 @@ app.get('/questions', async (req, res, next) => {
     next();
 })
 
+
 let questionAnswered = [];
 // route to get a random question from the array of questions
 app.get('/questions/random', async (req, res, next) => {
@@ -35,19 +36,24 @@ app.get('/questions/random', async (req, res, next) => {
     let number = Math.floor(Math.random() * questions.length);
     let exists = true;
 
+    // if the array of answered questions is the same as the questions array then all questions have been tried, reset the answered questions array and start over
     if(questionAnswered.length === questions.length) {
         questionAnswered = [];
     }
 
+    // if the questions array doesn't exist or there are no questions in the array then send an error
     if(!questions || !questions.length) {
         res.status(404).send();
     }
 
+    // if there is nothing in the answered questions array, skip the while loop and move on
     if(!questionAnswered.length) {
         exists = false;
     }
     
+    // while loop that checks to see if the question has already been sent recently, if so then get a new question then check again
     while(exists) {
+        // if the id of the currrent question trying to be sent is in the answered array, change which question is trying to be sent and check again
         if(questionAnswered.includes(questions[number].id)) {
             number = Math.floor(Math.random() * questions.length); 
         } else {
@@ -55,6 +61,7 @@ app.get('/questions/random', async (req, res, next) => {
         }
     }
 
+    // send the question to the client and add the id to the answered array for checks when GET request is called again
     questionAnswered.push(questions[number].id);
     res.status(200).send(questions[number]);
     next();
