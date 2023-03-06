b06c66ffb8dc03b4e0593b25922fcd135eef826b
Lewis Takavarasha <110342079+Sonagi-R@users.noreply.github.com>
2023-03-06 14:05:25 +0000

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const app = require("./server.js");
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 // make server listen on the specified port
 app.listen(port, () => {
