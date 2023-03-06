694f5d6679d7637b517c228c5f81d1f8a63ed2f8
lewis <lewis.takavarasha@gmail.com>
2023-03-06 14:54:29 +0000

    add timer function for web page timer

--- client/index.js
+++ client/index.js
@@ -1,1 +1,14 @@
 console.log("Hello World!");
+const timer = document.querySelector('#timer')
+
+let countDown = 59
+
+const timerCountDown = () => {
+for (let i = 0; i < 60; i++){
+    setTimeout(() => {
+        timer.textContent = `0:${countDown}`
+        countDown -= 1
+    },i*1000)
+}
+}
+
